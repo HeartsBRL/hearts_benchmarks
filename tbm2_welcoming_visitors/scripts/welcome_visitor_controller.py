848759bfb6722fc4351e27dfc0219f23bcb15d16
Lizzie <lizzie.stone@hotmail.com>
2018-09-23 00:06:48 +0100

    Add comments for other visitors, add TODOs, remove unused functions

--- tbm2_welcoming_visitors/scripts/welcome_visitor_controller.py
+++ tbm2_welcoming_visitors/scripts/welcome_visitor_controller.py
@@ -49,35 +49,36 @@ class Controller():
         self.has_scan_changed = False
         rospy.Subscriber("/scan_change", String, self.scan_changed_callback)
         
-        while not self.has_scan_changed:
+        while not self.has_scan_changed: #TODO add timer to request person to move to better viewing position if difficulty detecting? Might affect dr kimble code...
             rospy.sleep(1)
            
         # TODO: kill subscriber 
     
     def detect_visitor(self):
+        #TODO figure out what this all does and comment it
         self.votes = [ ]
         sub = rospy.Subscriber("/hearts/face/user", String, self.face_callback)
         rospy.loginfo("subscribed to topic /hearts/face/user")
-        rospy.sleep(20)
+        rospy.sleep(20) #TODO this is a huge delay for every time a face needs to be detected!
         sub.unregister()
         rospy.loginfo("unsubscribed from topic /hearts/face/user - votes = " + str(len(self.votes)))
         counts = Counter(self.votes)
         rospy.loginfo(str(counts))
         visitors = counts.most_common(2)
-        visitor = None
+        likliest_visitor = None
         if len(visitors) == 2:
             visitor1 = visitors[0]
             visitor2 = visitors[1]
             
             if visitor1[1] != visitor2[1]:
-                visitor = visitor1[0]
+                likliest_visitor = visitor1[0]
         elif len(visitors) == 1:
-            visitor = visitors[0][0]
+            likliest_visitor = visitors[0][0]
         
-        if not visitor is None: 
-            rospy.loginfo("visitor = " + visitor)
+        if likliest_visitor is not None: 
+            rospy.loginfo("visitor = " + likliest_visitor)
         
-        return visitor
+        return likliest_visitor
         
     def face_callback(self, msg):
         rospy.loginfo("face_callback: " + msg.data)
@@ -86,13 +87,7 @@ class Controller():
     #def voice_callback(self, data):
     #    rospy.loginfo("voice_callback: " + data.data)
     #    self.current_voice = data.data
- 
-    #def location_result_callback(self, data):
-    #    rospy.loginfo("location_result_callback") 
   
-#   def state_callback(self, data):
-#        rospy.loginfo("state_callback")
-
     def benchmark_state_callback(self, data):
         if data.benchmark_state == BenchmarkState.STOP:
             rospy.loginfo("STOP")
@@ -144,13 +139,6 @@ class Controller():
             rospy.loginfo("detected unrecognized person")
             self.process_face_unrecognized()  
 
-    #def state_callback(self, data):
-    #    rospy.loginfo("state_callback: " + data.data)
-    #    if self.detect_faces == False and self.bell < data.bell:
-    #        self.bell = data.bell
-    #        # bell pressed, detect face
-    #        self.detect_faces = True
-    
     def say(self, text):
         rospy.loginfo("saying \"" + text + "\"")
         rospy.sleep(1)
@@ -192,9 +180,10 @@ class Controller():
         self.location_result = data.data
 
     def process_face_postman(self):
-                
+        # Door should be open already
+        
         # 3.b speak to the postman
-        self.say("I will receive the post mail, please stand back while I move my arm") 
+        self.say("Hello postman, I will receive the post mail, please stand back while I move my arm") 
         
         #TODO move to receive pose
         
@@ -234,15 +223,13 @@ class Controller():
             return
 
     def process_face_deliman(self):
+        self.say("Hello deliman")
         
         # 3. speak to the deliman, request him to open the door: "I am here, please open the door"
-        self.say("I will receive the breakfast, please open the door")
-
-        # 4. detect door is open
-        rospy.sleep(3)
+        self.say("I will receive the breakfast")
 
         # 5. speak to the deliman, instruct to follow robot: "Please follow me"
-        self.say("Hello! Please follow me")
+        self.say("Please follow me to the kitchen")
 
         # 6. move to kitchen
         if self.move_to("home", 3) == False:
@@ -250,14 +237,14 @@ class Controller():
             return
 
         # 7. speak to the deliman, instruct to leave breakfast box on the table: "Please leave the breakfast box on the table"
-        self.say("Please leave the breakfast box on the table")
-# topic: /y/compressed
+        self.say("Please leave the breakfast box on the table") #TODO check if this will still be a breakfast box
+        
         # 8. wait
-        # TODO
+        # TODO check if it has been done?
         rospy.sleep(5)
         
         # 9. speak to the deliman, instruct to follow robot: "Please follow me"
-        self.say("Please follow me")
+        self.say("Thank you, please follow me back to the door")
 
         # 10. move to front door
         if self.move_to("hallway", 3) == False:
@@ -266,6 +253,7 @@ class Controller():
 
         # 11. bid deliman farewell
         self.say("Thank you for visiting. Goodbye!")
+        #TODO close door/ask deliman to close door
 
         # 12. return to base
         if self.move_to("home", 3) == False:
@@ -277,13 +265,7 @@ class Controller():
 
     def process_face_doctor(self):
         # 1. speak to the doctor, "Hi Dr. Kimble, I am coming to open the door."
-        self.say("Hello Dr. Kimble, please open the door")
-
-        # 2. detect door is open
-        rospy.sleep(3)
-
-        # 3. speak to the doctor, instruct to follow robot: "Please follow me"
-        self.say("Please follow me")
+        self.say("Hello Doctor Kimble, please come in and shut the door behind you. I will guide you to Granny Annie") #TODO check what names are appropriate?
 
         # 4. move to bedroom
         if self.move_to("outside bedroom", 3) == False:
@@ -291,23 +273,23 @@ class Controller():
             return
 
         # 5. speak to doctor, advise robot will wait
-        self.say("Please enter. Stand facing me when you are done.")
+        self.say("Please enter, I will wait here. Stand facing me when you are ready to leave.")
     
         # 6. wait until doctor exits the bedroom
-        rospy.sleep(3)
-        self.wait_for_scan_changed()
+        rospy.sleep(3) #TODO is this here to give dr time to move out of sight?
+        self.wait_for_scan_changed() # waits to detect face again
         self.say("Now you are done. I will follow you to the door. Please lead the way.")
-                
-        #self.say("Now you are done.")
         rospy.sleep(2)
         
-        # 9. move to hallway
-        if self.move_to("hallway", 3) == False:
+        # 9. move to hallway 
+        if self.move_to("hallway", 3) == False: #TODO follow dr, don't just run them over!
             self.say("I am unable to move to the front door")
             return
             
         # 10. bid farewell
-        self.say("Thank you for visiting. Goodbye!")
+        self.say("Thank you for visiting. Please close the door behind you. Goodbye!")
+
+        #TODO any way to detect door is closed?
 
         # 11. return to base
         if self.move_to("home", 3) == False:
@@ -316,7 +298,7 @@ class Controller():
 
     def process_face_unrecognized(self):
         # 1. speak to visitor, "Sorry, I don't know you. I cannot open the door."
-        self.say("Sorry, I don't know you. I cannot open the door") # TODO door will be opened first, so shut door now
+        self.say("Sorry, I don't recognize you. I cannot let you in. Please close the door.") # TODO door will be opened first, so shut door now
         
         if self.move_to("home", 3) == False:
             self.say("I am unable to move to the base")
