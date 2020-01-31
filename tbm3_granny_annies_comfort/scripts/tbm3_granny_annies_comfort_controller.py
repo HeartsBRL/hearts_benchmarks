a6af97333c5e5fcfde91d151a48977aad32808d2
derek Ripper <derek@firstcottage.com>
2020-01-31 17:06:06 +0000

    TBM3 - backup - almost theregit status Derek

--- tbm3_granny_annies_comfort/scripts/tbm3_granny_annies_comfort_controller.py
+++ tbm3_granny_annies_comfort/scripts/tbm3_granny_annies_comfort_controller.py
@@ -2,7 +2,7 @@
 ##############################################################################################
 # Author  : Derek Ripper/Joe Daly
 # Created : Dec 2017
-# Purpose : To fulfil ERL-SR competition Bench Mark  - Grannie Annies comfort
+# Purpose : To fulfil ERL-SR competition Bench Mark  - TBM3 Grannie Annies comfort
 #           Written for the 22-26 Jan 2018 ERL competition in Edinburgh.
 #
 ##############################################################################################
@@ -14,6 +14,16 @@
 # Jan/Feb 2019 - BRL competition
 #         - more debugging for logic errors
 # Oct 2019 - Revist TBM3 correct logic , add GA postion to data from Tablet
+#
+# Jan 2020 - Derek
+#           Remove the frigs that allowed us to stop using subscribers for listening for
+#           Commands and Anwsers. These were put in to try and bug fix at BRL for the FEB 2019
+#           ERL competition. With hindsight we went down the wring track! and blamed the 
+#           introduction of the generic controller for listening issues.
+#
+#           ALSO - have remove all code relating to the Rulebook for TBM3 as used in the last
+#           competition in Edinburgh Jan 2018. Switching on lights,operating blinds etc,etc,
+#           was dropped from the Rulebook for Madrid Oct 2018 onwards.
 #         - 
 #############################################################################################
 import rospy
@@ -30,9 +40,6 @@ import python_support_library.text_colours as TC
 import language_v2 as nlp
 
 prt = TC.tc() # generally used instead of print stmt to colorise messages appropriately
-######
-# NB "self.speech" is defined by the stt_callback code in the generic controller.
-#####
 
 class ControllerTBM3(GenericController):
 
@@ -44,26 +51,12 @@ class ControllerTBM3(GenericController):
         self.pub_twist =   rospy.Publisher('/mobile_base_controller/cmd_vel', Twist,  queue_size=10)
         self.pose_2d_pub = rospy.Publisher('hearts/navigation/goal',          Pose2D, queue_size=10)
 
-        #Subscribers
-        # self.listen4cmd('on') # DAR probably remove these 4 items
-        # self.listen4cmd('off')
-        # self.listen4ans('on')
-        # self.listen4ans('off') # why?
-
         rospy.Subscriber("roah_rsbb/benchmark/state", BenchmarkState, self.benchmark_state_callback)
 
         #Services
         self.prepare =              rospy.ServiceProxy('/roah_rsbb/end_prepare',          std_srvs.srv.Empty)
         self.execute =              rospy.ServiceProxy('/roah_rsbb/end_execute',          std_srvs.srv.Empty)
         self.user_location_service =rospy.ServiceProxy('/roah_rsbb/tablet/map',           std_srvs.srv.Empty)
-        self.switch_2_on_service =  rospy.ServiceProxy('/roah_rsbb/devices/switch_2/on',  std_srvs.srv.Empty)
-        self.switch_2_off_service = rospy.ServiceProxy('/roah_rsbb/devices/switch_2/off', std_srvs.srv.Empty)
-        self.switch_1_on_service =  rospy.ServiceProxy('/roah_rsbb/devices/switch_1/on',  std_srvs.srv.Empty)
-        self.switch_1_off_service = rospy.ServiceProxy('/roah_rsbb/devices/switch_1/off', std_srvs.srv.Empty)
-        self.dimmer_set_service =   rospy.ServiceProxy('/roah_rsbb/devices/dimmer/set',   Percentage)
-        self.blinds_max_service =   rospy.ServiceProxy('/roah_rsbb/devices/blinds/max',   std_srvs.srv.Empty)
-        self.blinds_min_service =   rospy.ServiceProxy('/roah_rsbb/devices/blinds/min',   std_srvs.srv.Empty)
-        self.blinds_set_service =   rospy.ServiceProxy('/roah_rsbb/devices/blinds/set',   Percentage)
 
         #ROS parameters
         self.IROBOT = rospy.get_param("robot_in_use")
@@ -75,18 +68,6 @@ class ControllerTBM3(GenericController):
         # speech processing and load up competition data
         self.analysis = nlp.Analysis(self)
         self.analysis.getcompdata()
-      
-        # Granny Annies position in our map's coord system
-        # col 0= bed, col1 = sofa
-        # self.ulocx = [ -0.493560373783 ,-2.34082365036]
-        # self.ulocy = [ -3.79606962204,  -7.20651531219]
-        # self.uloct = [  0.995374783353,  0.92682136867]
-
-        #test version
-        #self.ulocx = [ 0.206475734711, 2.52560305595 ]
-        #self.ulocy = [ 0.507319450378, 0.0467052459717 ]
-        #self.uloct = [ 0.00143957138062, 0.00225162506104 ]
-
 
         # Granny Annies position in judges coord system
         # h for high value and l for low value ie the range
@@ -95,121 +76,38 @@ class ControllerTBM3(GenericController):
         self.jlocyh = [-0.50, -4.20]
         self.jlocyl = [-7.20, -5.50]
 
-
-        # Disable head manager
-        #?head_mgr = NavigationCameraMgr()
-        #?head_mgr.head_mgr_as("disable")
-
-        # List of unwanted words
-        self.rm_words = [
-        'a',
-        'of',
-        'in',
-        'the',
-        'please',
-        'my',
-        'to',
-        'me'    # nb trailing space to avoid corrupting "home"
-        ]
-
-        # List of words that ALL mean get
-        self.get_words = [
-        'bring',
-        'find',
-        'fetch'
-        ]
-
-        # Dictionary for commands to robot
-        self.actions_dict = {
-        "switch on left light bedroom"  : "self.on_LLB()",
-        "switch off left light bedroom" : "self.off_LLB()",
-        "switch on right light bedroom" : "self.on_RLB()",
-        "switch off right light bedroom": "self.off_RLB()",
-        "switch on both lights bedroom" : "self.on_BLB()" ,
-        "switch off both lights bedroom": "self.off_BLB()",
-        "set light dimmer half"         : "self.half_L()" ,
-        "open blinds"                   : "self.open_B()" ,
-        "close blinds"                  : "self.close_B()",
-        "leave blinds half open"        : "self.half_B()" ,
-        "go home"                       : "self.go_home()",
-        "get cardboard box"             : "self.get('cardboard box')",
-        "get coca-cola can"             : "self.get('coca-cola can')",
-        "get coca cola can"             : "self.get('coca-cola can')",
-        "get mug"                       : "self.get('mug')",
-        "get candle"                    : "self.get('candle')",
-        "get cup"                       : "self.get('cup')",
-        "get reading glasses"           : "self.get('reading glasses')"
-
-        }
-
-        # Dictionary for OBJECTS to be recognised at a location. The x,y,theta of these locations
-        # must be recorded in the locations.json file.
-        self.object_dict = {
-        "cardboard box"   : ["kitchen counter",
-                                "kitchen table",
-                                "coffee table",
-                                "bedside table"
-                            ],
-        "coca-cola can"   : ["kitchen table"],
-        "mug"             : ["kitchen counter"],
-        "candle"          : ["coffee table"],
-        "cup"             : ["kitchen table"],
-        "reading glasses" : ["bedside table"],
-        "candle"          : ["coffee table"]
-        }
-
     def listen4cmd(self,status):
-        prt.debug("listen4cmd - revised code")
-        print('***** Listening for a COMMAND')
+
         if status == 'on' :
-            self.toggle_stt('on')
-
-            while self.speech == None:
-                print("##### in loop for COMMAND")
-                rospy.sleep(0.1)
-
-            self.toggle_stt("off")
-
-            speech2text = self.speech
-
-            prt.debug("CMD speech2text:\n "+str(speech2text)+"\n" )
-            self.heardCommand(speech2text)
-
-
-            #self.sub_cmd=rospy.Subscriber("/hearts/stt", String, self.hearCommand_callback)
+            prt.info('***** Listening for a COMMAND')
+            #self.toggle_stt('on')
+            self.sub_cmd=rospy.Subscriber("/hearts/stt", String, self.heardCommand_callback)
+            #self.toggle_stt('off')
 
         else:
-            self.toggle_stt('off')
-            print('***** NOT! Listening for a COMMAND')
-            #self.sub_cmd.unregister()
-
+            #self.toggle_stt('off')
+            prt.info('***** NOT! Listening for a COMMAND')
+            self.sub_cmd.unregister()
 
         return
 
     def listen4ans(self,status):
         if status == 'on' :
-            print('***** Listening for an ANSWER revised code')
-            self.toggle_stt('on')
-
-            while self.speech == None:
-                print("##### in loop for answer")
-                rospy.sleep(0.1)
-
-            self.toggle_stt("off")
-
-            speech2text = self.speech
-            prt.debug("ANS speech2text: "+str(speech2text) )
-            #self.sub_ans=rospy.Subscriber("/hearts/stt", String, self.hearAnswer_callback)
-            self.heardAnswer(speech2text)
+            prt.info('***** Listening for an ANSWER revised code')
+            #self.toggle_stt('on')
+            prt.debug("##### In listen4ans: before subscriber")
+            self.sub_ans=rospy.Subscriber("/hearts/stt", String, self.heardAnswer_callback)
+            prt.debug("##### In listen4ans: after  subscriber")
+            #self.toggle_stt("off")
 
         else:
-            self.toggle_stt('off')
-            #self.sub_ans.unregister()
-            print('***** NOT! Listening for an ANSWER')
+            #self.toggle_stt('off')
+            self.sub_ans.unregister()
+            prt.info('***** NOT! Listening for an ANSWER')
 
         return
 
-    def heardAnswer(self,data):
+    def heardAnswer_callback(self,data):
         speech = str(data)
         speech = speech.lower()
         speech = speech.replace('"','')
@@ -225,12 +123,12 @@ class ControllerTBM3(GenericController):
             self.analysis.executeobjectives(self.theobjectives)
 
             self.say("The tasks are now completed to your satisfaction we trust")
-            prt.todo(" **************Stop Program here!*****************")
+            prt.todo(" **************Stop Program nicely  here!*****************")
             prt.warning("TBM3 controller: now quitting python!")
             quit()
-            # re-establish subscribers
+            # Turn off subscribing to any /hearts/stt topics
             self.listen4ans('off')
-            self.listen4cmd('on')
+            self.listen4cmd('off')
 
         elif 'no' in words:
             self.say("OK I will forget your last command. Please give me another command")
@@ -244,7 +142,8 @@ class ControllerTBM3(GenericController):
             self.listen4ans('on')
         return
 
-    def heardCommand(self,data):
+    def heardCommand_callback(self,data):
+        self.listen4cmd('off')
         prt.debug("in heardcommand_callback - speech: "+str(data))
         speech = str(data)
         speech = speech.lower()
@@ -260,16 +159,6 @@ class ControllerTBM3(GenericController):
             self.say("Sorry, no words were recognised. Please repeat.")
             self.listen4cmd('on')
             return
-
-        # lookupkey = self.bld_lookupkey(speech)
-        # rospy.loginfo("*** lookup key: "+lookupkey)
-
-        # # obtain  "Directive to Robot" from dictionary
-        # self.code2exec = self.actions_dict.get(lookupkey)
-
-        # # check that lookup key was found
-        # if self.code2exec != None:
-        #     #listen for "answer"
 
         ###### NEW CODE for speech processing    ######
 
@@ -299,10 +188,15 @@ class ControllerTBM3(GenericController):
 
             prt.info("command count rtn to GA controller = "+str(commandcount))
             prt.info("***** Tiago's confirmation to Granny Annie is: \n"+talkback+"\n")
-            self.say("You requested that I "+talkback+'. Shall I do this now?')
+            self.say("You requested that I "+talkback)
 
-            self.listen4cmd('off')
+            prt.info("Asking for YES or NO now!")
+            self.say("Shall I do this now?")
             self.listen4ans('on')
+
+            #dar self.listen4cmd('off')
+
+
         else:
             prt.info("command count = "+str(commandcount)+" so cannot proceed")
             txtcmds = self.num2text(commandcount)
@@ -322,126 +216,6 @@ class ControllerTBM3(GenericController):
             txt = "more than three commands"
 
         return txt
-
-
-    def bld_lookupkey(self,speech):
-
-        # Change words for fetching to "get"
-        rspeech = speech # Reduced words in captured speech
-        for sub_item in self.get_words:
-            rspeech = rspeech.replace(sub_item,'get')
-
-        # build list of words
-        words = rspeech.split(' ')
-
-        # change 2nd and subsequent values: 'off' to ''
-        firstfound = True
-        for idx, sub_item in enumerate(words):
-            if sub_item == 'off' and firstfound:
-                firstfound   = False
-            elif sub_item == 'off' and not firstfound:
-                words[idx] = ''
-
-        #remove unwanted words
-        for rm_item in self.rm_words:
-            for idx,elem in enumerate(words):
-                if elem == rm_item:
-                    words[idx] = ''
-
-        # rebuild lookup key as a string with single spaces
-        lookupkey = ''
-        for ii in range(1,len(words)):
-            if words[ii] != '':
-                lookupkey = lookupkey + words[ii]+' '
-        lookupkey = lookupkey.strip()
-
-        return lookupkey
-
-    # exec def's for DEVICE actions
-    def on_LLB(self):
-        # ON  Left Light Bedroom
-        self.switch_2_on_service()
-        return
-
-    def off_LLB(self):
-        # OFF Left Light Bedroom
-        self.switch_2_off_service()
-        return
-
-    def on_RLB(self):
-        # ON   Right Light Bedroom
-        self.switch_1_on_service()
-        return
-
-    def off_RLB(self):
-        # OFF Right Light Bedroom
-        self.switch_1_off_service()
-        return
-
-    def on_BLB(self):
-        # ON  Both Light Bedroom
-        self.on_LLB()
-        self.on_RLB()
-        return
-
-    def off_BLB(self):
-        # OFF Both Light Bedroom
-        self.off_LLB()
-        self.off_RLB()
-        return
-
-        # set light dimmer to half
-    def half_L(self):
-        print("\n***** in dimmer call\n")
-        percent = 50
-        self.dimmer_set_service(percent)
-
-    def open_B(self):
-        # OPEN Blinds
-        self.blinds_max_service()
-        return
-
-    def close_B(self):
-        # CLOSE Blinds
-        self.blinds_min_service()
-        return
-
-    def half_B(self):
-        # HALF CLOSE Blinds as at 27 Dec2017 does not work - percentae type problem
-        percent = 50
-        self.blinds_set_service(percent)
-        return
-
-    def go_home(self):
-        print("\n************ write code to send me home!!\n")
-        print("*****  I have gone HOME (Idiling Position)!\n")
-        self.say("OK,  I am returning to my Initial home position now")
-        self.move_to_location("home",1) #TODO check number of retries
-
-        return
-
-    def get(self,object):
-        # use object to look up location
-        location  = self.object_dict.get(object)
-        nlocations = len(location)
-        print('n locations = '+str(nlocations))
-        print('Location is = ')
-        print(location)
-
-        for LOC in location:
-            print("\n***** For object : "+object+" - Location is : "+LOC+"\n")
-            print("***** Go there now.")
-            self.move_to_location(LOC,1) #TODO check number of retries #robot moves to corresponding position according to locations.json file in hearts_navigation
-
-            print("***** Recognise object")
-            self.say("I can see the "+object+" so now returning to granny annie")
-
-            print("***** return to GA \n")
-            self.move_to_pose2D(self.user_location)
-            # cardboard box detection not working!
-            return
-
-        return
 
     ### When receiving a message from the "roah_rsbb/benchmark/state" topic, 
     ### will then publish the corresponding state to "roah_rsbb/messages_save"
@@ -526,6 +300,7 @@ class ControllerTBM3(GenericController):
         prt.result(" theta : "+str(self.user_location.theta))
 
         return
+
     ## Navigation Functions
     def move_to_pose2D(self, target_location_2D):
         ##publish granny annie's location
@@ -534,23 +309,6 @@ class ControllerTBM3(GenericController):
         self.pose_2d_pub.publish(target_location_2D)
 
         self.wait_to_arrive(5)
-
-#        ## Interactions
- #   def say(self, text):
-  #      delayconst  = 0.1 #seconds per character
-   #     nchars      = len(text)
-    #    delay = delayconst * nchars
-#
- #       # prt.debug("speech is: "+text)
-  #      # prt.debug("nchars is: "+str(nchars))
-   #     prt.debug("Delay for speech in seconds = "   +str(delay))
-#
- #       prt.todo("Remove def say from TBM3 controller - use generic def say instead: ")
-  #      #rospy.sleep(1)
-   #     self.tts_pub.publish(text)
-    #    rospy.sleep(delay+1)
-     #   prt.debug("sleep set to be proportional to num of chars - to see if truncation stops??")
-
 
     def device_operationsself(self):
         pass
@@ -646,33 +404,7 @@ class ControllerTBM3(GenericController):
         self.listen4cmd('on')
         prt.error("dropped out of def main")
         rospy.loginfo("End of MAIN programme")
-    ### taken from generic controller
-    ###
-    ### extract content of speech recognition
-    # def stt_callback(self,data): #from tbm3
-    #     '''
-    #     to use in code use self.speech
-    #     e.g. answer = self.speech
-    #     '''
-    #     speech = str(data.data)
-    #     speech = speech.lower()
-    #     prt.debug(" in Dereks sst_callback")
-    #     prt.debug("in TBM3 : GA controller overloaded stt - unedited speech follows....")
-    #     prt.debug(str(speech))
-    #     # remove the ROS msg "Data:" value from speech
-    #     item = 'data:'
-    #     if item in speech:
-    #         speech = speech.replace(item,'')
-    #         rospy.loginfo('*** Heard speech:\n'+speech+'\n')
 
-    #     # switch off as already handled by other code
-    #     # check that text has been returned
-    #     # if "bad_recognition" in speech:
-    #     #     self.say("Sorry, no words were recognised.")
-
-    #    self.speech = speech
-
-    ### listen for a specific word in SPEECH
 
 if __name__ == '__main__':
 
@@ -680,5 +412,4 @@ if __name__ == '__main__':
     rospy.init_node('annies_comfort', anonymous=False)
     rospy.loginfo("annies comfort controller has started")
     controller = ControllerTBM3()
-    #controller.main()
     rospy.spin()
